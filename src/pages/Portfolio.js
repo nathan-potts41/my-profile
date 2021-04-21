ec9162dae70715144283676a126fa52eb22817fb
nathan-potts41 <pottsandpans41@hotmail.com>
2021-04-20 22:28:00 -0500

    functional app, now css time

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,17 +2,19 @@ import React from 'react';
 import GertenHomesImg from '../assets/images/gerten-homes-screenshot.png';
 import ProMeasureImg from '../assets/images/promeasureScreenShot.png';
 import RunBuddyImg from '../assets/images/run-BuddyScreenShot.png';
-import WhateverWorksImg from '../assets/images/WhateverWorks2.0.png'
+import WhateverWorksImg from '../assets/images/WhateverWorks2.0.png';
+import BookSearchImg from '../assets/images/BookSearchScreenShot.png';
+import DeepThoughtsImg from '../assets/images/DeepThoughtsScreenShot.png';
 
 function Portfolio() {
     return (
         <div>
-            {/* <div>
-                Book Search
-                <img src={ }>
-                    <a href="https://sheltered-garden-15985.herokuapp.com/"></a>
-                </img>
-            </div> */}
+            <div>
+                <a href="https://sheltered-garden-15985.herokuapp.com/">
+                    Book Search
+                    <img src={BookSearchImg}></img>
+                </a>
+            </div>
             <div>
                 <a href="https://whatever-works-2.herokuapp.com/">
                     Whatever Works 2.0
@@ -31,12 +33,12 @@ function Portfolio() {
                     <img src={ProMeasureImg} alt="Screenshot of Promeasure"></img>
                 </a>
             </div>
-            {/* <div>
-                Deep Thoughts
-                <img src={ }>
-                    <a href="https://thawing-sierra-78607.herokuapp.com/"></a>
-                </img>
-            </div> */}
+            <div>
+                <a href="https://thawing-sierra-78607.herokuapp.com/">
+                    Deep Thoughts
+                    <img src={DeepThoughtsImg}></img>
+                </a>
+            </div>
             <div>
                 <a href="https://gertenhomes.herokuapp.com/">
                     Gerten Homes Realty
