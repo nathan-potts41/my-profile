b3dc30cf7877ca768d7149efca29b57ef75e81f4
nathan-potts41 <pottsandpans41@hotmail.com>
2021-05-07 15:45:03 -0500

    rough draft of project

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -8,15 +8,15 @@ function SideNav() {
                 <div>
                     <Link to="/about" className="waves-effect waves-teal btn-flat nav-tab">About Me</Link>
                 </div>
-                <div class="divider"></div>
+                <div className="divider"></div>
                 <div>
                     <Link to="/portfolio" className="nav-tab waves-effect waves-teal btn-flat">Portfolio</Link>
                 </div>
-                <div class="divider"></div>
+                <div className="divider"></div>
                 <div>
                     <Link to="/contact" className="nav-tab waves-effect waves-teal btn-flat">Contact</Link>
                 </div>
-                <div class="divider"></div>
+                <div className="divider"></div>
                 <div>
                     <Link to="/resume" className="nav-tab waves-effect waves-teal btn-flat">Resume</Link>
                 </div>
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,12 +5,23 @@ import resume from '../assets/resume/codingResume.pdf';
 function Resume() {
 
     return (
-        <div>
-            <Document
-                file={resume}
-            >
-                <Page pageNumber={1} />
-            </Document>
+        <div className="col s12 m10 l10 offset-m2 offset-l2 container-fluid">
+            <div className="row">
+                <div className="col s12 m6 l6">
+                    <Document
+                        file={resume}>
+                        <Page pageNumber={1} />
+                    </Document>
+                </div>
+                <br></br>
+                <div className="col s12 m5 l5 offset-m1 offset-l1">
+                    <div className="header">
+                        Click the button below to download my resume.
+                        <a href={resume} download="Resume" class="waves-effect waves-light btn yellow darken-3">Download Resume</a>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
